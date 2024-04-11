d77abb982f8d867197deed4c39942416a92e3c8d
Oleg Kosenko <oleg.kosenko@brainlab.com>
2024-04-11 15:46:33 +0200

    Refactor main

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,33 @@ mod field;
 
 use crate::bot::alina::AlinaBot;
 use crate::bot::bot1::Bot1;
+use crate::bot::bot2::Bot2;
+use crate::bot::bot3::Bot3;
+use crate::bot::marching::MarchingBot;
 use crate::bot::random::RandomBot;
+use crate::bot::three_step::ThreeStepBot;
 use crate::bot::Bot;
 use crate::field::{Field, ShotResult};
 use std::env;
 use std::thread::sleep;
 use std::time::Duration;
-use crate::bot::bot2::Bot2;
-use crate::bot::bot3::Bot3;
-use crate::bot::marching::MarchingBot;
-use crate::bot::three_step::ThreeStepBot;
 
-fn play(draw: bool, bot: &mut dyn Bot) -> i32 {
+fn get_bot(bot_name: &str) -> Box<dyn Bot> {
+    match bot_name {
+        name if name == "RandomBot" => Box::new(RandomBot::new()),
+        name if name == "AlinaBot" => Box::new(AlinaBot::new()),
+        name if name == "Bot1" => Box::new(Bot1::new()),
+        name if name == "Bot2" => Box::new(Bot2::new()),
+        name if name == "Bot3" => Box::new(Bot3::new()),
+        name if name == "MarchingBot" => Box::new(MarchingBot::new()),
+        name if name == "ThreeStepBot1" => Box::new(ThreeStepBot::new(1)),
+        name if name == "ThreeStepBot2" => Box::new(ThreeStepBot::new(2)),
+        name if name == "ThreeStepBot3" => Box::new(ThreeStepBot::new(3)),
+        _ => Box::new(RandomBot::new()),
+    }
+}
+
+fn play(draw: bool, bot: &mut Box<dyn Bot>) -> i32 {
     let mut field = Field::new();
     field.fill();
 
@@ -51,118 +66,32 @@ fn main() {
     let mut testnum = 1000;
 
     if args.len() > 1 {
-        match args[1] {
-            _ if args[1] == "random" => {
-                let moves = play(true, &mut RandomBot::new());
-                println!("RandomBot won in {moves} moves");
-                return;
-            }
-            _ if args[1] == "alina" => {
-                let moves = play(true, &mut AlinaBot::new());
-                println!("AlinaBot won in {moves} moves");
-                return;
-            }
-            _ if args[1] == "bot1" => {
-                let moves = play(true, &mut Bot1::new());
-                println!("Bot1 won in {moves} moves");
-                return;
-            }
-            _ if args[1] == "bot2" => {
-                let moves = play(true, &mut Bot2::new());
-                println!("Bot2 won in {moves} moves");
-                return;
-            }
-            _ if args[1] == "bot3" => {
-                let moves = play(true, &mut Bot3::new());
-                println!("Bot3 won in {moves} moves");
-                return;
-            }
-            _ if args[1] == "marching" => {
-                let moves = play(true, &mut MarchingBot::new());
-                println!("MarchingBot won in {moves} moves");
-                return;
-            }
-            _ if args[1] == "three_step" => {
-                if args.len() > 2 {
-                    let steps = args[2].clone().parse().unwrap();
-                    let moves = play(true, &mut ThreeStepBot::new(steps));
-                    println!("ThreeStepBot with {steps} steps won in {moves} moves");
-                }
-                return;
-            }
-            _ => testnum = args[1].parse().unwrap_or(testnum),
-        }
+        let moves = play(true, &mut get_bot(&args[1]));
+        println!("{} won in {moves} moves", args[1]);
     }
+
+    let mut bots: Vec<&str> = vec![
+        "RandomBot",
+        "AlinaBot",
+        "Bot1",
+        "Bot2",
+        "Bot3",
+        "MarchingBot",
+        "ThreeStepBot1",
+        "ThreeStepBot2",
+        "ThreeStepBot3",
+    ];
 
     let mut vec = Vec::with_capacity(testnum);
-    for _ in 0..testnum {
-        vec.push(play(false, &mut RandomBot::new()));
-    }
-    let sum: i32 = vec.iter().sum();
-    let avg = sum as f64 / (vec.len() as f64);
-    println!("RandomBot won in {avg} moves on average");
 
-    vec.clear();
-    for _ in 0..testnum {
-        vec.push(play(false, &mut AlinaBot::new()));
-    }
-    let sum: i32 = vec.iter().sum();
-    let avg = sum as f64 / (vec.len() as f64);
-    println!("AlinaBot won in {avg} moves on average");
-
-    vec.clear();
-    for _ in 0..testnum {
-        vec.push(play(false, &mut Bot1::new()));
-    }
-    let sum: i32 = vec.iter().sum();
-    let avg = sum as f64 / (vec.len() as f64);
-    println!("Bot1 won in {avg} moves on average");
-
-    vec.clear();
-    for _ in 0..testnum {
-        vec.push(play(false, &mut Bot2::new()));
-    }
-    let sum: i32 = vec.iter().sum();
-    let avg = sum as f64 / (vec.len() as f64);
-    println!("Bot2 won in {avg} moves on average");
-
-    vec.clear();
-    for _ in 0..testnum {
-        vec.push(play(false, &mut Bot3::new()));
-    }
-    let sum: i32 = vec.iter().sum();
-    let avg = sum as f64 / (vec.len() as f64);
-    println!("Bot3 won in {avg} moves on average");
-
-    vec.clear();
-    for _ in 0..testnum {
-        vec.push(play(false, &mut MarchingBot::new()));
-    }
-    let sum: i32 = vec.iter().sum();
-    let avg = sum as f64 / (vec.len() as f64);
-    println!("MarchingBot won in {avg} moves on average");
-
-    vec.clear();
-    for _ in 0..testnum {
-        vec.push(play(false, &mut ThreeStepBot::new(1)));
-    }
-    let sum: i32 = vec.iter().sum();
-    let avg = sum as f64 / (vec.len() as f64);
-    println!("ThreeStepBot with 1 steps won in {avg} moves on average");
-
-    vec.clear();
-    for _ in 0..testnum {
-        vec.push(play(false, &mut ThreeStepBot::new(2)));
-    }
-    let sum: i32 = vec.iter().sum();
-    let avg = sum as f64 / (vec.len() as f64);
-    println!("ThreeStepBot with 2 steps won in {avg} moves on average");
-
-    vec.clear();
-    for _ in 0..testnum {
-        vec.push(play(false, &mut ThreeStepBot::new(3)));
-    }
-    let sum: i32 = vec.iter().sum();
-    let avg = sum as f64 / (vec.len() as f64);
-    println!("ThreeStepBot with 3 steps won in {avg} moves on average");
+    bots.iter().for_each(|bot_name| {
+        for _ in 0..testnum {
+            let mut bot = get_bot(bot_name.clone());
+            vec.push(play(false, &mut bot));
+        }
+        let sum: i32 = vec.iter().sum();
+        let avg = sum as f64 / (vec.len() as f64);
+        vec.clear();
+        println!("{} won in {avg} moves on average", bot_name);
+    });
 }
